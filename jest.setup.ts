bdbfbb4d454a76934351a191bb1606c1e53e7599
Sebastian Poma <73948362+Pomalec@users.noreply.github.com>
2025-09-09 16:04:16 +0100

    Add mocks for react-d3-cloud-modern and IntersectionObserver
    Implemented to Jest setup mock 'react-d3-cloud-modern', which ignores the import in order to test the project.
    Additional mock implementation for 'IntersectionObserver', this is due to browser APIS that are not present in jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,19 @@
 import '@testing-library/jest-dom';
+import React from 'react';
+jest.mock('react-d3-cloud-modern', () => ({
+  __esModule: true,
+  default: () => React.createElement('div', null, 'Mocked WordCloud'),
+}));
+
+class IntersectionObserverMock {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserverMock,
+})
